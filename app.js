31eb4a18215594d965faf1032df0b5749009f070
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-21 14:49:40 -0500

    Add Garage class

--- app.js
+++ app.js
@@ -28,3 +28,34 @@ class Motorcycle extends Vehicle {
     return "VROOM!!!";
   }
 }
+
+// Part Four
+class Garage {
+  constructor(capacity) {
+    this.capacity = capacity;
+  }
+  vehicles = [];
+  add(vehicle) {
+    if (!(vehicle instanceof Vehicle)) {
+      throw new Error("Only vehicles are allowed in here!");
+    } else if (this.vehicles.length >= this.capacity) {
+      throw new Error("Sorry, we're full.");
+    } else {
+      console.log("Vehicle added!");
+      return this.vehicles.push(vehicle);
+    }
+  }
+}
+
+let garage = new Garage(2);
+console.log(garage.vehicles); // []
+console.log(garage.add(new Car("Hyundai", "Elantra", 2015))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car]
+console.log(garage.add("Taco")); // "Only vehicles are allowed in here!"
+
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2000)));
+// "Vehicle added!"
+console.log(garage.vehicles); // [Car, Motorcycle]
+
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2001)));
+// "Sorry, we're full."
